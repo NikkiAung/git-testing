66585d1d0657a9951a7a46d902941765ba4733bc
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-05-07 14:47:48 -0700

    Added subtraction feature

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-def apply(a,b, operation):
+def apply(a, b, operation):
     if operation == "+":
         return a + b
+    elif operation == "-":
+        return a - b
 
-print(apply(1,2,"+"))
+print(apply(1, 2, "+"))
+print(apply(5, 3, "-"))
